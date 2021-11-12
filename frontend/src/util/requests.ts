7c2388d3a66c79916f548d3f2c67d244a4610c69
Marcos <marcossilva7@outlook.com.br>
2021-11-12 11:55:49 -0300

    Atualizando

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080'
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://dashboard.heroku.com/apps/sds3-marco'
